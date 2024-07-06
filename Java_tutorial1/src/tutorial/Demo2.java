9fd4ef53bcb618808abee32a79cec15881520d56
AdityaMishra <AdityaMishra@aditya>
2024-07-06 22:55:08 +0530

    first change

---
+++ Java_tutorial1/src/tutorial/Demo2.java
@@ -0,0 +1,16 @@
+package tutorial;
+
+public class Demo2 {
+	int i;
+	static Demo2 obj;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		obj=new Demo2();
+		System.out.println(obj.i);
+		obj.test();
+	}
+	public void test() {
+		System.out.println(obj.i);
+	}
+
+}
