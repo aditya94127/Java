853dcb741513a3ad11eaadb0332d798e642aa8fa
AdityaMishra <AdityaMishra@aditya>
2024-07-06 20:56:30 +0530

    day-2

---
+++ Java_tutorial1/src/tutorial/Demo1.java
@@ -0,0 +1,16 @@
+package tutorial;
+
+public class Demo1 {
+	int i=10;
+	static int j=20;
+	public static void main(String[] args) {
+		System.out.println(j);
+		Demo1 obj1=new Demo1();
+		obj1.test1();
+	}
+	public  void test1() {
+		System.out.println(Demo1.j);
+	}
+ 
+}
+ 
